da2ecaa73ec83fac64e4162b4cd1bfff6acf33cc
Mink <ntn.mink@gmail.com>
2022-06-22 19:25:01 +0700

    mr

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -16,20 +16,20 @@ function Header() {
 
   return (
     <>
-      {offset > 80 && location.pathname === "/" ? (
-        <div className="w-full sticky top-6 ">
-          <div className="rounded-full flex justify-between items-center mx-4 bg-gray-100 mt-6 py-3 px-8  ">
+      {location.pathname === "/" && offset < 80 ? (
+        <div className="w-full fixed ">
+          <div className="h-24 flex justify-between items-center mx-4 ">
             <Link to="/">
-              <img src={logoColor} alt="logo" to="/" />
+              <img src={logo} alt="logo" to="/" />
             </Link>
             <HeaderMenu />
           </div>
         </div>
       ) : (
-        <div className="w-full fixed ">
-          <div className="h-24 flex justify-between items-center mx-12 ">
+        <div className="w-full top-6 z-50 ">
+          <div className="rounded-full flex justify-between items-center mx-4 bg-gray-100 mt-6 py-3 px-8">
             <Link to="/">
-              <img src={logo} alt="logo" to="/" />
+              <img src={logoColor} alt="logo" to="/" />
             </Link>
             <HeaderMenu />
           </div>
