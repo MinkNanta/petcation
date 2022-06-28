e7617aca8e60a147102f7cd2e54fa08fa94bd6a1
amorntep <amorntep.mechaniceng@gmail.com>
2022-06-28 16:32:52 +0700

    add-api-user

--- src/components/profile/ProfilePage.js
+++ src/components/profile/ProfilePage.js
@@ -4,6 +4,7 @@ import Input from '../../common/Input';
 import { AddressContext } from '../../contexts/AddressContext';
 import { AuthContext, useAuth } from '../../contexts/AuthContext';
 import axios from '../../config/axios';
+import { updateUser } from '../../api/user';
 
 export default function ProfilePage() {
   const [province, setProvince] = useState('');
@@ -21,12 +22,6 @@ export default function ProfilePage() {
     useContext(AddressContext);
 
   const { User } = useContext(AuthContext);
-  // console.log(firstName);
-  // console.log(lastName);
-  // console.log(phoneNumber);
-  // console.log(email);
-  // console.log(zipCode);
-  // console.log(address);
 
   console.log(province);
   console.log(district);
@@ -37,119 +32,127 @@ export default function ProfilePage() {
   console.log(zipCode);
 
   // console.log(dropdownAddress);
+
+  const handleSubmit = async () => {
+    // await updateUser({ firstName: 'not' });
+  };
   return (
     <>
-      <div className="flex justify-between items-center ">
-        <h4 className="text-2xl">Information</h4>
-        <div className="flex gap-4">
-          <button className="btn-text">Cancel</button>
-          <button className="btn-small ">Save</button>
+      <form onSubmit={handleSubmit}>
+        <div className="flex justify-between items-center ">
+          <h4 className="text-2xl">Information</h4>
+          <div className="flex gap-4">
+            <button className="btn-text">Cancel</button>
+            <button className="btn-small " type="submit">
+              Save
+            </button>
+          </div>
         </div>
-      </div>
 
-      <div className="flex gap-3">
-        <Input
-          value={firstName}
-          label="First name"
-          onChange={(e) => setFirstNamne(e.target.value)}
-          placeholder="Enter your first name"
-          errMsg="Error Massage"
-          error={true}
-        />
-        <Input
-          value={lastName}
-          label="Last Name"
-          onChange={(e) => setLastNamne(e.target.value)}
-          placeholder="Enter your last name"
-          errMsg="Error Massage"
-          error={true}
-        />
-      </div>
+        <div className="flex gap-3">
+          <Input
+            value={firstName}
+            label="First name"
+            onChange={(e) => setFirstNamne(e.target.value)}
+            placeholder="Enter your first name"
+            errMsg="Error Massage"
+            error={true}
+          />
+          <Input
+            value={lastName}
+            label="Last Name"
+            onChange={(e) => setLastNamne(e.target.value)}
+            placeholder="Enter your last name"
+            errMsg="Error Massage"
+            error={true}
+          />
+        </div>
 
-      <div className=" ">
-        <Input
-          value={phoneNumber}
-          label="Phone Number"
-          onChange={(e) => setPhoneNumber(e.target.value)}
-          placeholder="Enter your phone number"
-          errMsg="Error Massage"
-          error={true}
-        />
-        <Input
-          value={email}
-          label="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter your email"
-          errMsg="Error Massage"
-          error={true}
-        />
-      </div>
+        <div className=" ">
+          <Input
+            value={phoneNumber}
+            label="Phone Number"
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            placeholder="Enter your phone number"
+            errMsg="Error Massage"
+            error={true}
+          />
+          <Input
+            value={email}
+            label="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            errMsg="Error Massage"
+            error={true}
+          />
+        </div>
 
-      {/* Province */}
-      <div>
-        <InputDropdown
-          label="Province"
-          onChange={(e) => {
-            setProvince(e.target.value);
-            getDstricts(e.target.value);
-          }}
-          errMsg="Error Massage"
-          error={true}
-        >
-          {dropdownAddress.provinces.map((province) => (
-            <option value={province.id}>{province.nameEn}</option>
-          ))}
-        </InputDropdown>
+        {/* Province */}
+        <div>
+          <InputDropdown
+            label="Province"
+            onChange={(e) => {
+              setProvince(e.target.value);
+              getDstricts(e.target.value);
+            }}
+            errMsg="Error Massage"
+            error={true}
+          >
+            {dropdownAddress.provinces.map((province) => (
+              <option value={province.id}>{province.nameEn}</option>
+            ))}
+          </InputDropdown>
 
-        <InputDropdown
-          label="District"
-          onChange={(e) => {
-            setDistrict(e.target.value);
-            getSubDstricts(e.target.value);
-          }}
-          errMsg="Error Massage"
-          error={true}
-        >
-          {dropdownAddress.districts?.map((district) => (
-            <option value={district.id}>{district.nameEn}</option>
-          ))}
-        </InputDropdown>
+          <InputDropdown
+            label="District"
+            onChange={(e) => {
+              setDistrict(e.target.value);
+              getSubDstricts(e.target.value);
+            }}
+            errMsg="Error Massage"
+            error={true}
+          >
+            {dropdownAddress.districts?.map((district) => (
+              <option value={district.id}>{district.nameEn}</option>
+            ))}
+          </InputDropdown>
 
-        <InputDropdown
-          label="Subdistrict"
-          onChange={(e) => {
-            setSubDistrict(e.target.value);
-          }}
-          errMsg="Error Massage"
-          error={true}
-        >
-          {dropdownAddress.subDistricts?.map((subdistrict) => (
-            <option value={subdistrict.zipCode}>{subdistrict.nameEn}</option>
-          ))}
-        </InputDropdown>
+          <InputDropdown
+            label="Subdistrict"
+            onChange={(e) => {
+              setSubDistrict(e.target.value);
+            }}
+            errMsg="Error Massage"
+            error={true}
+          >
+            {dropdownAddress.subDistricts?.map((subdistrict) => (
+              <option value={subdistrict.zipCode}>{subdistrict.nameEn}</option>
+            ))}
+          </InputDropdown>
 
-        <Input
-          value={subdistrict}
-          label="Postal/Zip Code (Optional)"
-          onChange={(e) => setZipCode(e.target.value)}
-          placeholder="Enter your zipcode"
-          errMsg="Error Massage"
-          error={true}
-        >
-          {/* {dropdownAddress.subDistricts?.map((subdistrict) => (
+          <Input
+            value={subdistrict}
+            label="Postal/Zip Code (Optional)"
+            onChange={(e) => setZipCode(e.target.value)}
+            placeholder="Enter your zipcode"
+            errMsg="Error Massage"
+            error={true}
+          >
+            {/* {dropdownAddress.subDistricts?.map((subdistrict) => (
             <option value={subdistrict.zipCodes}>{subdistrict.zipCodes}</option>
           ))} */}
-        </Input>
+          </Input>
 
-        <Input
-          value={address}
-          label="Address"
-          onChange={(e) => setAddress(e.target.value)}
-          placeholder="Enter your address"
-          errMsg="Error Massage"
-          error={true}
-        />
-      </div>
+          <Input
+            value={address}
+            label="Address"
+            onChange={(e) => setAddress(e.target.value)}
+            placeholder="Enter your address"
+            errMsg="Error Massage"
+            error={true}
+          />
+        </div>
+      </form>
     </>
   );
 }
