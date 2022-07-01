97ed131921cf3f04a39b95a48b29c0aaf3a22ced
Mink <ntn.mink@gmail.com>
2022-07-01 14:04:02 +0700

    not

--- src/components/main/Hero.js
+++ src/components/main/Hero.js
@@ -1,21 +1,85 @@
 // import React from 'react';
+import { SearchIcon } from '@heroicons/react/solid';
+import { useState } from 'react';
 import hero from '../../assets/img/hero.png';
 import Input from '../../common/Input';
 
 export default function Hero() {
+  const [where, setWhere] = useState('');
+  const [when, setWhen] = useState('');
+  const [pet, setPet] = useState('');
+  const [change, setChange] = useState(false);
+
+  console.log(where);
+  console.log(change);
+
   return (
-    <div className="relative h-[90vh] overflow-hidden z-0">
-      <div className="w-screen top-0">
-        <img src={hero} alt="hero" className="w-full" />
-      </div>
-      <div className="top-0 absolute w-screen h-[90vh]">
-        <div className="mainContainer h-full flex flex-col mt-40 content-center">
-          <h1 className="text-6xl text-white">
-            Find <span className="text-orange-500">Perfect</span> <br />
-            Place for Your Pet.
-          </h1>
-          <Input />
+    <div className="backgroundHero ">
+      <div className="mainContainer h-[90vh]">
+        <div className="h-2/5 "></div>
+        <h1 className="text-6xl text-white mb-4">
+          Find <span className="text-orange-500">Perfect</span> <br />
+          Place for Your Pet.
+        </h1>
+        <div className="flex ">
+          <div className=" bg-white rounded-full px-6 py-4 place-content-start flex">
+            <div className="flex gap-4">
+              <input
+                className="inputWhite"
+                placeholder="Where"
+                value={where}
+                // onChange={(e) => setWhere(e.target.value)}
+                onChange={(e) => {
+                  setWhere(e.target.value);
+                  setChange(true);
+                }}
+              />
+              <div className="line"></div>
+              <input
+                placeholder="When"
+                className="inputWhite"
+                value={when}
+                onChange={(e) => setWhen(e.target.value)}
+              />
+              <div className="line"></div>
+              <input
+                placeholder="With"
+                className="inputWhite"
+                value={pet}
+                onChange={(e) => setPet(e.target.value)}
+              />
+            </div>
+
+            <div className="bg-orange-500 w-9 h-9 rounded-full centerAll">
+              <SearchIcon className="w-6 h-6 m-auto text-white" />
+            </div>
+          </div>
         </div>
+        {change && (
+          <div className="flex gap-4">
+            <h1>hello not</h1>
+            <input
+              className="inputWhite"
+              placeholder="Where"
+              value={where}
+              onChange={(e) => setWhere(e.target.value)}
+            />
+            <div className="line"></div>
+            <input
+              placeholder="When"
+              className="inputWhite"
+              value={when}
+              onChange={(e) => setWhen(e.target.value)}
+            />
+            <div className="line"></div>
+            <input
+              placeholder="With"
+              className="inputWhite"
+              value={pet}
+              onChange={(e) => setPet(e.target.value)}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
